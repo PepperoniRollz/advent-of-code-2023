7a1ec40c1fa95282ab503ea9cc19aed4057ae65d
pepperonirollz <cfitzpatrick616@gmail.com>
2024-09-21 20:06:05 -0400

    day one

---
+++ day1/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"github.com/pepperonirollz/aoc-2023/pkg/utils"
+)
+
+func main() {
+	solveDayOne("../inputs/day1-1.txt")
+	solvePart2("../inputs/day1-1.txt")
+}
+
+func solveDayOne(filename string) {
+	var digits []rune
+	sum := 0
+	lines := utils.Parse(filename)
+	for _, line := range lines {
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				digits = append(digits, char)
+			}
+		}
+		num, err := strconv.Atoi(string(digits[0]) + string(digits[len(digits)-1]))
+		if err != nil {
+			panic(err)
+		}
+		sum += num
+		digits = digits[:0]
+	}
+
+	fmt.Println("Part 1 sum: ", sum)
+
+}
+
+func solvePart2(filename string) {
+	digitMap := map[string]rune{
+		"one":   '1',
+		"two":   '2',
+		"three": '3',
+		"four":  '4',
+		"five":  '5',
+		"six":   '6',
+		"seven": '7',
+		"eight": '8',
+		"nine":  '9',
+		"zero":  '0',
+	}
+	sum := 0
+	lines := utils.Parse(filename)
+	for _, line := range lines {
+
+		indexMap := make(map[int]string)
+		var values []string
+		for word, _ := range digitMap {
+			start := 0
+			for {
+				index := strings.Index(line[start:], word)
+				if index == -1 {
+					break
+				}
+				realIndex := start + index
+				start = realIndex + len(word)
+				indexMap[realIndex] = word
+			}
+		}
+		for i, char := range line {
+			if unicode.IsDigit(char) {
+				indexMap[i] = string(char)
+			}
+		}
+		for i, _ := range line {
+			value, ok := indexMap[i]
+			if ok {
+				v, ok := digitMap[value]
+				if ok {
+					values = append(values, string(v))
+				} else {
+					values = append(values, value)
+				}
+			}
+
+		}
+
+		num, err := strconv.Atoi(string(values[0]) + string(values[len(values)-1]))
+		if err != nil {
+			panic(err)
+		}
+		sum += num
+
+		values = values[:0]
+	}
+	fmt.Println("Part 2 sum: ", sum)
+}
---
+++ pkg/utils/parser.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+)
+
+func Parse(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return lines
+}
